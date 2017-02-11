a72ca50e4719f70cdb80b5616b1a32672fb62ef3
helio.saraiva <buzato@hotmail.com>
2017-02-11 00:54:28 -0200

    replace5

--- cliente/admin.py
+++ cliente/admin.py
@@ -9,7 +9,6 @@ class ClienteAdmin(admin.ModelAdmin):
 	date_hierarchy = 'data_nascimento'#cria uma hieraquia de tempo, linha do tempo, consulta por data
 	list_display = ('nome', 'data_nascimento', 'salario', 'email', 'filhos' )#cria barras na colunas ordenaveis
 	list_filter = ('nome', 'data_nascimento', 'salario', 'email', 'filhos' )#cria menu de filtro ordenado
-	#readonly_fields = ('filhos',)#Deixa o campo setado somente leitura, não pode ser editado
 	search_fields = ('nome', 'data_nascimento', 'salario', 'email', 'filhos' )#cria barra de pequisa em cima da tabelas  
 
 
